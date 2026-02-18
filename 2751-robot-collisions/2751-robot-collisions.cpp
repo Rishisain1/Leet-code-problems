9a5cd8ba26626efa2b8df994e6efecfe739954c3
Rishisain1 <rishisaini604@gmail.com>
2026-02-18 18:22:11 +0530

    Time: 33 ms (99.23%), Space: 203.8 MB (46.94%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        set<pair<int,int>> set;
         int n=positions.size();
+
+// we are not going to use set because it take more time as compare to sort a vector not complexity wise but the constant factor of set is more because it implement using red black tree
+
+        // set<pair<int,int>> set; 
+        // for(int i=0;i<n;i++){
+        //     set.insert({positions[i],i});
+        // }
+
+        vector<pair<int,int>> sortedpos;
         for(int i=0;i<n;i++){
-            set.insert({positions[i],i});
+            sortedpos.push_back({positions[i],i});
         }
+        sort(sortedpos.begin(),sortedpos.end());
         stack<int> stack;
-        for(auto [position,i]:set){
+        for(auto [position,i]:sortedpos){
             if(directions[i]=='R'){
                 stack.push(i);                
             }
@@ -34,13 +43,19 @@ public:
 
             }
         }
-        int i=0;
-        for(int i=0;i<healths.size();i++){
-            if(healths[i]==-1){
-                healths.erase(healths.begin()+i);
-                i--;
+        // this way is very heavy so we use another array
+        // for(int i=0;i<healths.size();i++){
+        //     if(healths[i]==-1){
+        //         healths.erase(healths.begin()+i);
+        //         i--;
+        //     }
+        // }
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            if(healths[i]!=-1){
+                ans.push_back(healths[i]);
             }
         }
-        return healths;
+        return ans;
     }
 };
