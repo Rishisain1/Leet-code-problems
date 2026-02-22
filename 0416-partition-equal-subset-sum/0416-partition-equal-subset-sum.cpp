ec7c41bcd8dbb98739569c397379ecd62b65bf9f
Rishisain1 <rishisaini604@gmail.com>
2026-02-22 07:17:04 +0530

    Time: 121 ms (62.07%), Space: 111.1 MB (23.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -26,7 +26,22 @@ bool solve(vector<int> &nums,int sum,int n,vector<vector<int>> &cache){
         }
         if(sum%2)return false;
         sum=sum/2;
-        vector<vector<int>> cache(n,vector<int>(sum+1,-1));
-        return solve(nums,sum,n-1,cache);
+        // vector<vector<int>> cache(n,vector<int>(sum+1,-1));
+        // return solve(nums,sum,n-1,cache);
+         vector<vector<int>> cache(n+1,vector<int>(sum+1,0));
+        for(int i=0;i<=n;i++){
+            cache[i][0]=1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(j>=nums[i-1]){
+                    cache[i][j]=cache[i-1][j-nums[i-1]]||cache[i-1][j];
+                }
+                else{
+                   cache[i][j]=cache[i-1][j];
+                }
+            }
+        }
+        return cache[n][sum];
     }
 };
