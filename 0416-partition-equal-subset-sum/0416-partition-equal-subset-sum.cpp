b3972e055a8d59a953e20d8981da9197c179d011
Rishisain1 <rishisaini604@gmail.com>
2026-02-22 07:11:08 +0530

    Time: 44 ms (93.34%), Space: 110.4 MB (44.87%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
 
-// bool so
-
 //memoization
-bool solve(vector<int> &nums,int sum,int rem,int n,vector<vector<int>> &cache){
-    if(sum==rem){
+bool solve(vector<int> &nums,int sum,int n,vector<vector<int>> &cache){
+    if(sum==0){
         return 1;
     }
     if(n<0){
         return 0;
     }
     if(cache[n][sum]!=-1)return cache[n][sum];
-    return cache[n][sum]=solve(nums,sum-nums[n],rem+nums[n],n-1,cache)||solve(nums,sum,rem,n-1,cache);
+    if(sum>=nums[n]){
+        return cache[n][sum]=solve(nums,sum-nums[n],n-1,cache)||solve(nums,sum,n-1,cache);
+    }
+    else{
+        return cache[n][sum]=solve(nums,sum,n-1,cache);
+    }
 }
 
     bool canPartition(vector<int>& nums) {
@@ -21,7 +24,9 @@ bool solve(vector<int> &nums,int sum,int rem,int n,vector<vector<int>> &cache){
         for(int i=0;i<n;i++){
             sum+=nums[i];
         }
+        if(sum%2)return false;
+        sum=sum/2;
         vector<vector<int>> cache(n,vector<int>(sum+1,-1));
-        return solve(nums,sum,0,n-1,cache);
+        return solve(nums,sum,n-1,cache);
     }
 };
