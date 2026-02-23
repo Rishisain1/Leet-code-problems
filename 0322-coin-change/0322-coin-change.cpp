eb7166057de808c6d67cefdad77032fca4d84c4e
Rishisain1 <rishisaini604@gmail.com>
2026-02-23 18:59:38 +0530

    Time: 50 ms (26.77%), Space: 53 MB (12.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -36,9 +36,33 @@ int solve(vector<int>& coins, int amount,int i,vector<vector<int>>&dp){
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         // int ans=solve(coins,amount,n-1);
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans=solve(coins,amount,n-1,dp);
 
+
+        // //memoization
+        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        // int ans=solve(coins,amount,n-1,dp);
+
+
+        vector<vector<int>> dp(n+1,vector<int>(amount+1,INT_MAX-1));
+        for(int i=0;i<amount;i++){
+            dp[0][i]=INT_MAX-1;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j]=min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        int ans=dp[n][amount];
         if(ans==INT_MAX-1){
             return -1;
         }
