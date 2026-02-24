bced627ca7dbd9c2f61690b0a6b22eb4dc9d5c86
Rishisain1 <rishisaini604@gmail.com>
2026-02-24 10:43:31 +0530

    Time: 46 ms (36.3%), Space: 52.9 MB (16.94%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -17,43 +17,41 @@ public:
 //     return solve(coins,amount,i-1);
       
 // }
-int solve(vector<int>& coins, int amount,int i,vector<vector<int>>&dp){
-    if(amount==0){
-        return 0;
-    }
-    if(i<0){
-        return INT_MAX-1;
-    }
-    if(dp[i][amount]!=-1)return dp[i][amount];
-    if(coins[i]<=amount){
-        return dp[i][amount]=min(solve(coins,amount-coins[i],i,dp)+1,solve(coins,amount,i-1,dp));
-    }
+// int solve(vector<int>& coins, int amount,int i,vector<vector<int>>&dp){
+//     if(amount==0){
+//         return 0;
+//     }
+//     if(i<0){
+//         return INT_MAX-1;
+//     }
+//     if(dp[i][amount]!=-1)return dp[i][amount];
+//     if(coins[i]<=amount){
+//         return dp[i][amount]=min(solve(coins,amount-coins[i],i,dp)+1,solve(coins,amount,i-1,dp));
+//     }
     
-    return dp[i][amount]=solve(coins,amount,i-1,dp);
+//     return dp[i][amount]=solve(coins,amount,i-1,dp);
       
-}
+// }
 
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         // int ans=solve(coins,amount,n-1);
 
 
-        // //memoization
+         //memoization
         // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
         // int ans=solve(coins,amount,n-1,dp);
 
-
+        // dp
         vector<vector<int>> dp(n+1,vector<int>(amount+1,INT_MAX-1));
-        for(int i=0;i<amount;i++){
-            dp[0][i]=INT_MAX-1;
-        }
+        
         for(int i=0;i<=n;i++){
             dp[i][0]=0;
         }
 
 
         for(int i=1;i<=n;i++){
-            for(int j=0;j<=amount;j++){
+            for(int j=1;j<=amount;j++){
                 if(coins[i-1]<=j){
                     dp[i][j]=min(dp[i][j-coins[i-1]]+1,dp[i-1][j]);
                 }
