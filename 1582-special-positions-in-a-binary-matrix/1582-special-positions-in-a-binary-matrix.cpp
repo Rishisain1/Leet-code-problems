8eb41975e2102387e208640e076f4aa81b603d30
Rishisain1 <rishisaini604@gmail.com>
2026-03-06 12:40:30 +0530

    Time: 0 ms (100%), Space: 17 MB (61.52%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    static vector<int> check1(vector<int>& vect, int sz){
-        vector<int> ans;
-        for(int i=0; i<sz; i++){
-            if (vect[i]==1) ans.push_back(i);
-        }
-        return ans;
-    }
-    static int numSpecial(vector<vector<int>>& mat) {
-        int m=mat.size(), n=mat[0].size(), ans=0;
-        vector<bool> col(n, 0);//already check or not
-        for(int i=0; i<m; i++){
-            auto idx=check1(mat[i], n);
-            int j;
-            if (idx.size()==1 && col[(j=idx[0])]==0){
-                col[j]=1;
-                int count_col1=0;
-                for(int k=0; k<m; k++)
-                    count_col1+=mat[k][j];
-                if (count_col1==1){
-                //    cout<<"("<<i<<","<<j<<")\n";
-                    ans++;
-                }     
-            }
-            else{
-                for(int j: idx)
-                    col[j]=1;
+    int numSpecial(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<int> rows(n,0);
+        vector<int> cols(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]){
+                    rows[i]++;
+                    cols[j]++;
+                }
             }
         }
-        return ans;
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]){
+                    if(rows[i]-1)continue;
+                    if(cols[j]-1==0)count++;
+                }
+            }
+        }
+        return count;
     }
 };
