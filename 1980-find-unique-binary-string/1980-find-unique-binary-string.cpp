7a8d5d79ec88e91cad271bbb3589ad1037126c58
Rishisain1 <rishisaini604@gmail.com>
2026-03-08 08:34:55 +0530

    Time: 0 ms (100%), Space: 14.5 MB (14.61%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-
-void solve(int i,int n,unordered_map<string,int> &comb,string &temp){
+void solve(int i,int n,unordered_map<string,int> &comb,string & ans,string temp){
+    if(ans!="")return;
     if(i>=n){
-        comb[temp]=-1;
-        return ;
+        if(comb[temp]==0){
+            ans=temp;
+           
+        }
+         return ;
     }
-    string s=temp;
-    temp+='0';
-    solve(i+1,n,comb,temp);
-    temp=s;
-    temp+='1';
-    solve(i+1,n,comb,temp);
-    temp=s;
+    solve(i+1,n,comb,ans,temp+'0');
+    solve(i+1,n,comb,ans,temp+'1');
 }
+
 
     string findDifferentBinaryString(vector<string>& nums) {
         int n=nums.size();
         unordered_map<string,int> comb;
-        string temp="";
-        solve(0,n,comb,temp);
         for(auto s:nums){
             comb[s]=1;
         }
-        for(auto [s,f]:comb){
-            if(f==-1){
-                return s;
-            }
-        }
-        return "";
+        string ans="";
+        solve(0,n,comb,ans,"");
+        return ans;
     }
 };
