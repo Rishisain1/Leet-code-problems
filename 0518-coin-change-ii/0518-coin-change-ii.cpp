75347449f7b5140b024ad2b21f0d9a6b64ff57d2
Rishisain1 <rishisaini604@gmail.com>
2026-03-09 06:51:24 +0530

    Time: 5 ms (88.82%), Space: 10.1 MB (96.73%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -36,7 +36,22 @@ int solve(int amount,vector<int>&coins,int i,vector<vector<int>>&dp){
     int change(int amount, vector<int>& coins) {
         // return solve(amount,coins,0);
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return solve(amount,coins,0,dp);
+        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        // return solve(amount,coins,0,dp);
+
+        // we use unsigned int to avoid overflow even long long give overflow error
+        // 2d dp is work in this case but we have to use unsigned int 
+        vector<unsigned int> dp(amount + 1, 0);
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return (int)dp[amount];
+       
+        // return dp[n][amount];
     }
 };
