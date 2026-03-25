c111e5c7bc1ae6e0bc28dd5a01965f2d8ec1604c
Rishisain1 <rishisaini604@gmail.com>
2026-03-25 23:34:14 +0530

    Time: 136 ms (17.37%), Space: 34.9 MB (10.96%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -2,21 +2,22 @@ class Solution {
 public:
 
 bool solve(string s1,string s2,unordered_map<string,int> &dp){
-    if(dp[s1+s2])return (dp[s1+s2])%2;
+    string key=s1+'#'+s2;
+    if(dp[key])return (dp[key])%2;
     if(s1==s2){
-        dp[s1+s2]=1;
+        dp[key]=1;
         return true;}
     int n=s1.length();
     for(int i=0;i<n-1;i++){
         if(solve(s1.substr(0,i+1),s2.substr(n-i-1,i+1),dp) && solve(s1.substr(i+1,n-i-1),s2.substr(0,n-i-1),dp))        {
-            dp[s1+s2]=1;
+            dp[key]=1;
             return true;
         }
         if(solve(s1.substr(0,i+1),s2.substr(0,i+1),dp)&&solve(s1.substr(i+1,n-i-1),s2.substr(i+1,n-i-1),dp)){
-            dp[s1+s2]=1;
+            dp[key]=1;
             return true;}
     }
-    dp[s1+s2]=2;
+    dp[key]=2;
     return false;
 }
     bool isScramble(string s1, string s2) {
