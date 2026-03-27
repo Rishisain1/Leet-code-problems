4f718cf44c548b80d23a4fd340edccd294d25dd0
Rishisain1 <rishisaini604@gmail.com>
2026-03-27 13:55:01 +0530

    Time: 19 ms (5.59%), Space: 34 MB (5.59%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
-        int n=mat[0].size();
-        k=k%n;
-        if(k==0)return true;
-        for(auto arr:mat){
-            vector<int> nums=arr;
-            rotate(arr.begin(),arr.begin()+k,arr.end());
-            if(arr!=nums){
-                return false;
-            }
+        int n=mat.size();
+        int m=mat[0].size();
+        if(m==1){
+            return true;
         }
-        return true;
+        vector<vector<int>> ans=mat;
+        k=k%m;
+        
+            for(int i=0;i<n;i++){
+                int j=k;
+                vector<int> temp;
+                for(;j<m;j++){
+                    temp.push_back(mat[i][j]);
+                }
+                for(int x=0;x<k;x++){
+                    temp.push_back(mat[i][x]);
+                }
+                mat[i]=temp;
+                if(mat[i]!=ans[i]){
+                    return false;
+                }
+            }
+        
+    return true;
     }
 };
