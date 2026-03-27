3ade49ad531c3aa38cea3d3d35ebeaef23254a61
Rishisain1 <rishisaini604@gmail.com>
2026-03-27 13:52:40 +0530

    Time: 0 ms (100%), Space: 32.2 MB (36.03%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
     bool areSimilar(vector<vector<int>>& mat, int k) {
-        int n=mat.size();
-        int m=mat[0].size();
-        if(m==1){
-            return true;
-        }
-        vector<vector<int>> ans=mat;
-        k=k%m;
-        if(m%2==0){
-            for(int i=0;i<n;i++){
-                int j=k;
-                vector<int> temp;
-                for(;j<m;j++){
-                    temp.push_back(mat[i][j]);
-                }
-                for(int x=0;x<k;x++){
-                    temp.push_back(mat[i][x]);
-                }
-                mat[i]=temp;
+        int n=mat[0].size();
+        k=k%n;
+        if(k==0)return true;
+        for(auto arr:mat){
+            vector<int> nums=arr;
+            rotate(arr.begin(),arr.begin()+k,arr.end());
+            if(arr!=nums){
+                return false;
             }
         }
-        else{
-            for(int i=0;i<n;i++){
-                int j=m-k;
-                int z=j;
-                vector<int> temp;
-                for(;j<m;j++){
-                    temp.push_back(mat[i][j]);
-                }
-                for(int x=0;x<z;x++){
-                    temp.push_back(mat[i][x]);
-                }
-                mat[i]=temp;
-            }
-        }
-    return mat==ans;
+        return true;
     }
 };
