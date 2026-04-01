927c127780d5b8fc8f54ceb452eb974e7b58a45c
Rishisain1 <rishisaini604@gmail.com>
2026-04-02 00:02:20 +0530

    Time: 54 ms (71.06%), Space: 204.8 MB (32.73%) - LeetHub

--- 2751-robot-collisions/2751-robot-collisions.cpp
+++ 2751-robot-collisions/2751-robot-collisions.cpp
@@ -1,59 +1,54 @@
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        vector<pair<int,int>> arr;
         int n=positions.size();
-
-// we are not going to use set because it take more time as compare to sort a vector not complexity wise but the constant factor of set is more because it implement using red black tree
-
-        // set<pair<int,int>> set; 
-        // for(int i=0;i<n;i++){
-        //     set.insert({positions[i],i});
-        // }
-
-        vector<pair<int,int>> sortedpos;
         for(int i=0;i<n;i++){
-            sortedpos.push_back({positions[i],i});
+            arr.push_back({positions[i],i});
         }
-        sort(sortedpos.begin(),sortedpos.end());
+        sort(arr.begin(),arr.end());
         stack<int> stack;
-        for(auto [position,i]:sortedpos){
-            if(directions[i]=='R'){
-                stack.push(i);                
+        stack.push(-1);
+        for(auto [p,i]:arr){
+            int j=stack.top();
+            if(directions[i]=='R'||(j==-1&&directions[i]=='R')){
+                stack.push(i);
             }
-            else{// case for L
-                while(stack.size()>0){
-                    int j=stack.top();
-                    if(healths[j]>healths[i]){
-                        healths[j]--;
-                        healths[i]=-1;
-                        break;
+            else if(directions[i]=='L'){
+                if(j==-1){
+                    continue;
+                }
+                else if(healths[i]==healths[j]){
+                    healths[i]=-1;
+                    healths[j]=-1;
+                    stack.pop();
+                }
+                else if(healths[i]<healths[j]){
+                    healths[i]=-1;
+                    healths[j]-=1;
+                }
+                else{
+                    while(stack.top()!=-1&&healths[stack.top()]<healths[i]){
+                        healths[stack.top()]=-1;
+                         healths[i]-=1;
+                         stack.pop();
                     }
-                    else if(healths[j]==healths[i]){
-                        stack.pop();
-                        healths[j]=-1;
+                    if(stack.top()!=-1&&healths[stack.top()]>healths[i]){
                         healths[i]=-1;
-                        break;
+                        healths[stack.top()]-=1;
                     }
-                    else{
+                    else if(stack.top()!=-1&&healths[stack.top()]==healths[i]){
+                        healths[i]=-1;
+                        healths[stack.top()]=-1;
                         stack.pop();
-                        healths[j]=-1;
-                        healths[i]--;
                     }
                 }
-
             }
         }
-        // this way is very heavy so we use another array
-        // for(int i=0;i<healths.size();i++){
-        //     if(healths[i]==-1){
-        //         healths.erase(healths.begin()+i);
-        //         i--;
-        //     }
-        // }
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            if(healths[i]!=-1){
-                ans.push_back(healths[i]);
+        vector<int>ans;
+        for(auto h:healths){
+            if(h!=-1){
+                ans.push_back(h);
             }
         }
         return ans;
