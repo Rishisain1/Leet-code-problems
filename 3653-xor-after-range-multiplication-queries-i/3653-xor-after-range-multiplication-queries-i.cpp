7e92a586b313229dacb1900abe808f5d6cf8475b
Rishisain1 <rishisaini604@gmail.com>
2026-04-08 12:13:26 +0530

    Time: 113 ms (18.66%), Space: 169.3 MB (10.53%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
+        int mod=1e9+7;
+        for(auto q:queries){
+            long long s=q[0],e=q[1],k=q[2],v=q[3];
+            for(int i=s;i<=e;i=i+k){
+                nums[i]=((nums[i]*1LL)*v)%mod;
+            }
+        }
+        long long ans=nums[0];
+        for(int i=1;i<nums.size();i++){
+            ans^=nums[i];
+        }
+        return ans;
+    }
+};
