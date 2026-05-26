d8325404903b5a453d5df4565c6c579cd266ba2c
Rishisain1 <rishisaini604@gmail.com>
2026-05-26 14:18:01 +0530

    Time: 4 ms (91.61%), Space: 29.1 MB (28.52%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool canReach(string s, int minJump, int maxJump) {
+        int far=0;
         int n=s.length();
         vector<int> arr(n,0);
-        int rc=0;
         arr[0]=1;
-        for(int i=1;i<n;i++){
-            if(i-minJump>=0&&arr[i-minJump]){
-                rc++;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0)continue;
+            int start=max(minJump+i,far+1);
+            int end=min(i+maxJump,n-1);
+            for(int j=start;j<=end;j++){
+                if(s[j]=='0'){
+                    arr[j]=1;
+                }
             }
-            if(i-maxJump-1>=0&&arr[i-maxJump-1]){
-                rc--;
-            }
-            if(s[i]=='0'&&rc>0){
-                arr[i]=1;
-            }
+            far=max(end,far);
         }
         return arr[n-1];
     }
