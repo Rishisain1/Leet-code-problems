0dd08cfffe3d05e674fef786a164d4a34aeb90b4
Rishisain1 <rishisaini604@gmail.com>
2026-05-26 15:19:07 +0530

    Time: 123 ms (5.05%), Space: 19.1 MB (30.26%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,39 +1,51 @@
 class Solution {
 public:
-    int dfs(int i, vector<int>& arr, int d, vector<int>& dp) {
-        if (dp[i] != -1)
-            return dp[i];
 
-        int best = 1;
-        int n = arr.size();
-
-        for (int nxt = i + 1; nxt <= min(n - 1, i + d); nxt++) {
-            if (arr[nxt] >= arr[i])
-                break;
-
-            best = max(best, 1 + dfs(nxt, arr, d, dp));
+int solve(vector<int>& arr, int d,int i,vector<int>& dp){
+    int n=arr.size();
+    int x=max(0,i-d);
+    int y=min(n-1,i+d);
+    int left=1;
+    for(int j=i-1;j>=x;j--){
+        if(arr[i]<=arr[j])break;
+        if(dp[j]==-1){
+            left=max(solve(arr,d,j,dp)+1,left);
         }
-
-        for (int nxt = i - 1; nxt >= max(0, i - d); nxt--) {
-            if (arr[nxt] >= arr[i])
-                break;
-
-            best = max(best, 1 + dfs(nxt, arr, d, dp));
+        else{
+            left=max(dp[j]+1,left);
         }
-
-        return dp[i] = best;
     }
+    int right=1;
+    for(int j=i+1;j<=y;j++){
+        if(arr[i]<=arr[j])break;
+        if(dp[j]==-1){
+            right=max(solve(arr,d,j,dp)+1,right);
+        }
+        else{
+            right=max(right,dp[j]+1);
+        }
+    }
+    return dp[i]=max(left,right);
+
+}
 
     int maxJumps(vector<int>& arr, int d) {
-        int n = arr.size();
-        vector<int> dp(n, -1);
-
-        int ans = 1;
-
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, dfs(i, arr, d, dp));
+        int ans=1;
+        int n=arr.size();
+        vector<int> dp(n,-1);
+        for(int i=0;i<n;i++){
+            int count=1;
+            if(dp[i]==-1){
+                count=solve(arr,d,i,dp);
+            }
+            else{
+                count=dp[i];
+            }
+            ans=max(ans,count);       
         }
-
+        for(int i:dp){
+        cout<<i<<" ";
+        }
         return ans;
     }
 };
