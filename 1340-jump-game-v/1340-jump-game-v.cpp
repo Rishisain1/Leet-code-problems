9c4f0eb90d2b8d2cc5330b0dadba7d124285a3bf
Rishisain1 <rishisaini604@gmail.com>
2026-05-26 15:26:09 +0530

    Time: 137 ms (5.05%), Space: 19 MB (81.69%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
 
 int solve(vector<int>& arr, int d,int i,vector<int>& dp){
+    if(dp[i]!=-1)return dp[i];
     int n=arr.size();
     int x=max(0,i-d);
     int y=min(n-1,i+d);
     int left=1;
     for(int j=i-1;j>=x;j--){
         if(arr[i]<=arr[j])break;
-        if(dp[j]==-1){
+       
             left=max(solve(arr,d,j,dp)+1,left);
-        }
-        else{
-            left=max(dp[j]+1,left);
-        }
+        
     }
     int right=1;
     for(int j=i+1;j<=y;j++){
         if(arr[i]<=arr[j])break;
-        if(dp[j]==-1){
+        
             right=max(solve(arr,d,j,dp)+1,right);
-        }
-        else{
-            right=max(right,dp[j]+1);
-        }
+       
     }
     return dp[i]=max(left,right);
 
