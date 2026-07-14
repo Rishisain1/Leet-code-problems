90108dd662c27fd6cff3909d34b254aee6e412d6
Rishisain1 <rishisaini604@gmail.com>
2026-07-14 08:16:02 +0530

    Time: 0 ms (100%), Space: 10.9 MB (41.64%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -14,10 +14,14 @@ int maxf(vector<int> &freq){
         int ans=0;
         int i=0,j=0;
         int n=s.length();
+        // when we ignore if the freq become less or and window not in the solution
+        int maxfreq=INT_MIN;
         while(j<n){
             int ch=s[j]-'A';
             freq[ch]++;
-            int maxfreq=maxf(freq);
+            // int maxfreq=maxf(freq);
+            maxfreq=max(freq[ch],maxfreq);
+
             while(j-i+1-maxfreq>k){
                 int x=s[i]-'A';
                 freq[x]--;
