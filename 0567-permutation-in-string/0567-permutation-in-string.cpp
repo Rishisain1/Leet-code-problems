c95c702f45876d876d94f577d77a95a893209fe9
Rishisain1 <rishisaini604@gmail.com>
2026-08-08 16:32:43 +0530

    Time: 0 ms (100%), Space: 9.9 MB (70.76%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if(s1.size()>s2.size())return false;
+        if(s1.length()>s2.length())return false;
         vector<int> freq1(26,0);
         vector<int> freq2(26,0);
         for(auto ch:s1){
-            int i=ch-'a';
-            freq1[i]++;
+            int x=ch-'a';
+            freq1[x]++;
         }
-        int n=s1.length();
-        int j=0;
-        for(;j<n;j++){
-            int ch=s2[j]-'a';
-            freq2[ch]++;
+        int i=0,j=0,n=s1.length();
+        for(j=0;j<n;j++){// keep in mind we not suppose to redefine the j
+            int x=s2[j]-'a';
+            freq2[x]++;
         }
-        if(freq1==freq2){
-            return true;
-        }
-        int n2=s2.size();
-        int i=0;
+        if(freq1==freq2)return true;
+        int n2=s2.length();
         while(j<n2){
-            int ch=s2[i]-'a';
-            freq2[ch]--;
+            int x=s2[i]-'a';
+            freq2[x]--;
+            x=s2[j]-'a';
+            freq2[x]++;
+            if(freq1==freq2)return true;
             i++;
-            ch=s2[j]-'a';
-            freq2[ch]++;
             j++;
-            if(freq1==freq2){
-                return true;
-            }
+
         }
         return false;
     }
