8d130c54730dc448c1d2c16781654212e7b78e8e
Rishisain1 <rishisaini604@gmail.com>
2026-09-22 21:12:24 +0530

    Time: 7 ms (96.98%), Space: 44.2 MB (97.33%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
 
     int solve(vector<int>& nums,int k){
-        int i=0,j=0,n=nums.size();
+        int i=0,j=0,n=nums.size(),d=0;
         long long count=0;
-        unordered_map<int,int> freq;
+        vector<int> freq(n+1,0);// we use vector instead of map because the nums[i]is always in the range of 0 ans the nums.size();
         while(j<n){
+            if(freq[nums[j]]==0){
+                d++;
+            }
             freq[nums[j]]++;
-            while(freq.size()>k){
+            while(d>k){
                 freq[nums[i]]--;
                 if(freq[nums[i]]==0){
-                    freq.erase(nums[i]);
+                    d--;
                 }
                 i++;
             }
