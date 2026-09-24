5b0bb2f87412795c2e3500270b753cc00c4c605a
Rishisain1 <rishisaini604@gmail.com>
2026-09-24 19:38:30 +0530

    Time: 3 ms (7.06%), Space: 10.9 MB (37.68%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
 
-    int solve(vector<int>&nums,vector<int>&dp,int i,int n){
-        if(i>=n){
-            return 0;
+    // int solve(vector<int>&nums,vector<int>&dp,int i,int n){
+    //     if(i>=n){
+    //         return 0;
+    //     }
+    //     if(dp[i]!=-1)return dp[i];
+    //     return dp[i]=max(solve(nums,dp,i+1,n),solve(nums,dp,i+2,n)+nums[i]);
+    // }
+
+    int solve(vector<int>&nums,int i,int n){
+        int prev1=0,prev2=0;
+        for(;i<n;i++){
+            int curr=max(prev1,prev2+nums[i]);
+            prev2=prev1;
+            prev1=curr;
         }
-        if(dp[i]!=-1)return dp[i];
-        return dp[i]=max(solve(nums,dp,i+1,n),solve(nums,dp,i+2,n)+nums[i]);
+        return max(prev2,prev1);
     }
 
     int rob(vector<int>& nums) {
@@ -14,6 +24,7 @@ public:
         if(n==1)return nums[0];
         vector<int>dp1(n,-1);
         vector<int>dp2(n,-1);
-        return max(solve(nums,dp1,0,n-1),solve(nums,dp2,1,n));
+        // return max(solve(nums,dp1,0,n-1),solve(nums,dp2,1,n));
+        return max(solve(nums,1,n),solve(nums,0,n-1));
     }
 };
